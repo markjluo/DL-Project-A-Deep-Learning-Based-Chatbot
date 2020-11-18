fa22fcfcd62cf1ffa75e28b0c90bbc76e7850b93
Mark Luo <markluo@Marks-MacBook-Pro.local>
2020-11-17 20:23:30 -0500

    add predict.py modules

---
+++ predict.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+
+def evaluate(sentence):
+  sentence = preprocess_sentence(sentence)
+
+  sentence = tf.expand_dims(
+      START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
+
+  output = tf.expand_dims(START_TOKEN, 0)
+
+  for i in range(MAX_LENGTH):
+    predictions = model(inputs=[sentence, output], training=False)
+
+    # select the last word from the seq_len dimension
+    predictions = predictions[:, -1:, :]
+    predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+    # return the result if the predicted_id is equal to the end token
+    if tf.equal(predicted_id, END_TOKEN[0]):
+      break
+
+    # concatenated the predicted_id to the output which is given to the decoder
+    # as its input.
+    output = tf.concat([output, predicted_id], axis=-1)
+
+  return tf.squeeze(output, axis=0)
+
+
+def predict(sentence):
+  prediction = evaluate(sentence)
+
+  predicted_sentence = tokenizer.decode(
+      [i for i in prediction if i < tokenizer.vocab_size])
+
+  print('Input: {}'.format(sentence))
+  print('Output: {}'.format(predicted_sentence))
+
+  return predicted_sentence
